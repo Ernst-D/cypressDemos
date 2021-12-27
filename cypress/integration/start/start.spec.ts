2829e3c62faafec448ebd8b87f78ee48145d563b
Ernst-D <ernst.dzerevanko@gmail.com>
2021-12-27 13:52:20 +0300

    [fix] spelling

--- cypress/integration/start/start.spec.ts
+++ cypress/integration/start/start.spec.ts
@@ -3,6 +3,6 @@ import custom from "../../support/custom";
 describe("Test suite", ()=>{
     it("Test Case", ()=>{
         cy.visit("https://ej2.syncfusion.com/showcase/typescript/webmail");
-        custom.Command.Pause();
+        custom.Command.pause();
     });
 });
