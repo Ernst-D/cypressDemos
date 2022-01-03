4ce2417ee95e01115f4c92433c535a120d161ad1
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-03 18:13:23 +0300

    [cypress-rwa] added types for image snapshots

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,17 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const {
+  addMatchImageSnapshotPlugin,
+} = require('cypress-image-snapshot/plugin');
+
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  addMatchImageSnapshotPlugin(on, config);
   require("cypress-fail-fast/plugin")(on, config);
   return config;
 };
