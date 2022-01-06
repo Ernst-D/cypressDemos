6521bdf3f2aa2230084ed161f251e6f7a8fdb5dc
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-06 18:33:19 +0300

    [misc] minor changes

--- cypress/pages/cypress-rwa/login.page.ts
+++ cypress/pages/cypress-rwa/login.page.ts
@@ -2,6 +2,6 @@ import custom from "../../support/custom";
 
 export default {
     login(){
-        custom.Command.login({username:"Adolfo.Skiles80", password:"s3cret"});
+        custom.Command.login({username:"Roberto.Gulgowski5", password:"s3cret"});
     }
 };
