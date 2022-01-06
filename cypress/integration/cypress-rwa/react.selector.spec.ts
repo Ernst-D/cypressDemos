0b7c14b8b615f7e891562c8720bde39ef264655d
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-07 01:14:11 +0300

    [cypress-rwa] added react selector spec

---
+++ cypress/integration/cypress-rwa/react.selector.spec.ts
@@ -0,0 +1,22 @@
+/// <reference types="cypress-react-selector" />
+
+import homePage from "../../pages/cypress-rwa/home.page";
+import loginPage from "../../pages/cypress-rwa/login.page";
+import env from "../../support/env";
+
+describe("Cypress react selector test suite",() => {
+    describe("navigate to login page",() => {
+        before(() => {
+            cy.visit(env.cypressRWA.url.toString());
+            cy.waitForReact(1000); 
+        });
+        it("user login",{tags: ["@RWA", "@test", "@react"]},() => {
+            loginPage.login();
+            homePage.header.should("be.visible");
+            cy.react("ButtonBase",{props:{ "to":"/user/settings" }}).click();
+            cy.waitForReact();
+            cy.react('TextField', { props: { inputProps: { "data-test": "user-settings-firstName-input" } } }).clear();
+           
+        });
+    });
+});
