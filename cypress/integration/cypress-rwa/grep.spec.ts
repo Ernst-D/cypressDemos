bf9a141ebd604bb4959cc7de613cbf1b1de2f7f3
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-06 18:58:59 +0300

    [cypress-rwa] added grep test suite

--- cypress/integration/cypress-rwa/grep.spec.ts
+++ cypress/integration/cypress-rwa/grep.spec.ts
@@ -1,3 +1,6 @@
+/// <reference types="cypress-grep" />
+
+
 import homePage from "../../pages/cypress-rwa/home.page";
 import loginPage from "../../pages/cypress-rwa/login.page";
 import env from "../../support/env";
@@ -7,7 +10,7 @@ describe("Cypress grep test suite",() => {
         before(() => {
             cy.visit(env.cypressRWA.url.toString());
         });
-        it("@RWA-0.",() => {
+        it("user login",{tags: ["@RWA", "@test"]},() => {
             loginPage.login();
             homePage.header.should("be.visible");
             cy.xpath("//*[contains(@data-test,'transaction-receiver')]").eq(0).should("be.visible");
