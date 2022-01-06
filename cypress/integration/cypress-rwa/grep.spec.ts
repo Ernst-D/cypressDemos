9726d10bc22a4626b0bf0b9f7550dd9e3a4458f9
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-06 18:33:37 +0300

    [cypress-rwa] init cypress-grep

---
+++ cypress/integration/cypress-rwa/grep.spec.ts
@@ -0,0 +1,16 @@
+import homePage from "../../pages/cypress-rwa/home.page";
+import loginPage from "../../pages/cypress-rwa/login.page";
+import env from "../../support/env";
+
+describe("Cypress grep test suite",() => {
+    describe("naviagte to login page",() => {
+        before(() => {
+            cy.visit(env.cypressRWA.url.toString());
+        });
+        it("@RWA-0.",() => {
+            loginPage.login();
+            homePage.header.should("be.visible");
+            cy.xpath("//*[contains(@data-test,'transaction-receiver')]").eq(0).should("be.visible");
+        });
+    });
+});
