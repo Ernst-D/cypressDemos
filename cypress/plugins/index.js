418bd889df01b5e233cd53a774fca18eab97b351
Ernst-D <ernst.dzerevanko@gmail.com>
2022-01-07 17:46:23 +0300

    [cypress-rwa] added cucumber-preprocessor

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,8 +12,10 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
-const { configure: configureCypressTestingLibrary } = require("@testing-library/cypress")
-
+const { configure: configureCypressTestingLibrary } = require("@testing-library/cypress");
+const browserify = require('@cypress/browserify-preprocessor');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const resolve = require('resolve');
 
 /**
  * @type {Cypress.PluginConfig}
@@ -21,8 +23,17 @@ const { configure: configureCypressTestingLibrary } = require("@testing-library/
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   require('cypress-grep/src/plugin')(config)
+  require("cypress-fail-fast/plugin")(on, config);
+
   configureCypressTestingLibrary(config);
   addMatchImageSnapshotPlugin(on, config);
-  require("cypress-fail-fast/plugin")(on, config);
+
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: resolve.sync('typescript', { baseDir: config.projectRoot }),
+  };
+
+  on('file:preprocessor', cucumber(options));
+
   return config;
 };
