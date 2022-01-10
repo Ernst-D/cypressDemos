a66d543b627f965e60af30889ee4aa9bd75c71c0
Ernst-D <80358262+Ernst-D@users.noreply.github.com>
2022-01-11 00:30:13 +0300

    Feature "circle-ci-js-config" (#8)

---
+++ .circleci/dynamic/jobs/default.job.js
@@ -0,0 +1,19 @@
+const CircleCI = require("@circleci/circleci-config-sdk");
+const nodeExecutor = require("../executors/node.executor");
+
+const cypressDefault = new CircleCI.Job("Default_cypress_tests",nodeExecutor);
+cypressDefault
+.addStep(new CircleCI.commands.Checkout())
+.addStep(new CircleCI.commands.Run({
+    name:"Install packages",
+    command:"npm i"}
+))
+.addStep(new CircleCI.commands.Run({
+    name:"Run default cypress spec",
+    command:"npx cypress run --spec ./cypress/integration/1-getting-started/*"
+}))
+.addStep(new CircleCI.commands.StoreArtifacts({
+    path:"./cypress/videos"
+}));
+
+module.exports = cypressDefault;
---
+++ .circleci/dynamic/jobs/webmail.job.js
@@ -0,0 +1,19 @@
+const CircleCI = require("@circleci/circleci-config-sdk");
+const nodeExecutor = require("../executors/node.executor");
+
+const webmailJob = new CircleCI.Job("webmail_e2e_test",nodeExecutor);
+webmailJob
+.addStep(new CircleCI.commands.Checkout())
+.addStep(new CircleCI.commands.Run({
+    name:"Install packages",
+    command:"npm i"}
+))
+.addStep(new CircleCI.commands.Run({
+    name:"Run Webmail e2e",
+    command:"npm run e2e:webmail"
+}))
+.addStep(new CircleCI.commands.StoreArtifacts({
+    path:"./cypress/screenshots"
+}));
+
+module.exports = webmailJob;
