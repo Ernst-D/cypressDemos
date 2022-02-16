0820d54e27e6b06ca9f26894dd604735973d3361
Ernst-D <ernst.dzerevanko@gmail.com>
2022-02-16 18:57:28 +0300

    [migration] refactoring spec

--- cypress/integration/cypress-rwa/specs/transaction.spec.ts
+++ cypress/integration/cypress-rwa/specs/transaction.spec.ts
@@ -27,60 +27,56 @@ test.describe("Transaction test suite",() => {
             custom.Command.isMobile();
             cy(page).restoreLocalStorage();
         });
-
-        test("user logs in and sees its username on home page",async ({page}) => {
-            await cy(page).visit(env.cypressRWA.url.toString());
-            await loginPage.login(page);
-            pageShim.homePage(page).header;
-            await page.pause();
-        });
-        test("user creates request transaction",async ({page}) => {
-            pageShim.homePage(page).header;
-            await pageShim.homePage(page).newTransactionBtn.click();
-            await page.pause();
-            transactionPage.contact.userlistSearchInpt.type("Adrien33", {force:true});
-            transactionPage.contact.usersList.eq(0).click();
-            custom.Command.pause();
-            transactionPage.payment.amountInput.type("1");
-            transactionPage.payment.noteInput.type("test transaction note");
-            
-            cy(page).intercept("POST","/transactions").as(transactionAlias);
-
-            transactionPage.payment.requestBtn.click();
-
-            // cy(page).wait(cyAlias(transactionAlias)).should(({ request, response }) => {
-            //     transactionRes = response.body;
-            //     expect(request.method).toEqual('POST');
-            //     expect(response.statusCode).toEqual(200);
-            //     cy(page).wrap(transactionRes.transaction.id).as(transactionId);
-            //   });
-            // cy(page).get(cyAlias(transactionAlias))
-            // .its('response.body').then(response => {
-            //     cy(page).log(response);
-            // });
-            
-            transactionPage.complete.requestedTransactionContainer.invoke("text")
-            .then(text => {
-                cy(page).log(`The text of transaction is ${text}`);
-            })
-            .should("equal","Requested $1.00 for test transaction note");
-
-            // pageShim.homePage(page).logout();
-            cy(page).intercept("GET",`/${notificationsAlias}`).as(notificationsAlias);
-
-            custom.Command.login({username:"Adrien33", password:"s3cret"}, page);
-            // pageShim.homePage(page).header;
-            // pageShim.homePage(page).notificationIcon.click();
-
-            // cy(page).wait(cyAlias(notificationsAlias)).should(({ request, response }) => {
-            //     let notificationsRes = response.body;
-            //     expect(request.method).toEqual('GET');
-            //     expect(response.statusCode).toEqual(200);
-            //     cy(page).log(notificationsRes);
-            //     cy(page).get(cyAlias(transactionId)).then(id => {
-            //         expect(notificationsRes.results.shift().transactionId).equal(id);
-            //     });
-            // });
+        test("transaction spec",async ({page}) => {
+            await test.step("user logs in and sees its username on home page",async () => {
+                await cy(page).visit(env.cypressRWA.url.toString());
+                await loginPage.login(page);
+            });
+            await test.step("user creates request transaction",async () => {
+                await pageShim.homePage(page).newTransactionBtn.click();
+                await pageShim.transactionPage(page).contact.userlistSearchInpt.type("Adrien33");
+                await pageShim.transactionPage(page).contact.usersList.nth(1).click();
+                await pageShim.transactionPage(page).payment.amountInput.type("1");
+                await pageShim.transactionPage(page).payment.noteInput.type("test transaction note");
+                await page.pause();
+                cy(page).intercept("POST","/transactions").as(transactionAlias);
+    
+                await pageShim.transactionPage(page).payment.requestBtn.click();
+    
+                // cy(page).wait(cyAlias(transactionAlias)).should(({ request, response }) => {
+                //     transactionRes = response.body;
+                //     expect(request.method).toEqual('POST');
+                //     expect(response.statusCode).toEqual(200);
+                //     cy(page).wrap(transactionRes.transaction.id).as(transactionId);
+                //   });
+                // cy(page).get(cyAlias(transactionAlias))
+                // .its('response.body').then(response => {
+                //     cy(page).log(response);
+                // });
+                
+                await pageShim.transactionPage(page).complete.requestedTransactionContainer.invoke("text")
+                .then(text => {
+                    cy(page).log(`The text of transaction is ${text}`);
+                })
+                .should("equal","Requested $1.00 for test transaction note");
+    
+                // pageShim.homePage(page).logout();
+                cy(page).intercept("GET",`/${notificationsAlias}`).as(notificationsAlias);
+    
+                custom.Command.login({username:"Adrien33", password:"s3cret"}, page);
+                // pageShim.homePage(page).header;
+                // pageShim.homePage(page).notificationIcon.click();
+    
+                // cy(page).wait(cyAlias(notificationsAlias)).should(({ request, response }) => {
+                //     let notificationsRes = response.body;
+                //     expect(request.method).toEqual('GET');
+                //     expect(response.statusCode).toEqual(200);
+                //     cy(page).log(notificationsRes);
+                //     cy(page).get(cyAlias(transactionId)).then(id => {
+                //         expect(notificationsRes.results.shift().transactionId).equal(id);
+                //     });
+                // });
+            });
         });
     }); 
 });
